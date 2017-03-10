07f4a89c758ab2e38faaa6ca912177eeb51b6249
saurabh <saurabhsuman4797@gmail.com>
2017-03-11 01:13:18 +0530

    xor

---
+++ SPOJ/Classical/OLOLO - Onotole needs your help.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <sstream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+double eps=1e-8;
+typedef long long ll;
+typedef vector<int> vi;
+typedef std::vector<long long> vl;
+const ll mod=1000000007ll;
+#define sc(x) scanf("%d",&(x))
+#define scl(x) scanf("%lld",&(x))
+
+
+int main() 
+{	
+	int t;
+	scanf("%d",&t);
+	
+	int temp;
+	sc(temp);
+	for(int pp=0;pp<t-1;pp++)
+	{
+		int t1;
+		sc(t1);
+		temp=temp^t1;
+
+
+	}
+	cout<<temp<<endl;
+	
+}

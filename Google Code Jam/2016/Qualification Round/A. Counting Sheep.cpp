0f0fc6f7b0a2670809ef4ce21e653c6b1db24aa4
saurabh <saurabhsuman4797@gmail.com>
2017-04-24 22:18:15 +0530

    added files

---
+++ Google Code Jam/2016/Qualification Round/A. Counting Sheep.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<stdio.h>
+#include<set>
+using namespace std;
+
+#define sc(x) scanf("%d\n",&(x))
+
+int main(){
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt","w",stdout);
+	int t;
+	sc(t);
+	for (int z = 1; z <= t; z++){
+		int n;
+		sc(n);
+		if (n == 0)cout << "Case #" << z << ": " << "INSOMNIA" << endl;
+		else{
+			set<int> myset;
+			for (int i = 1; i <= 100; i++){
+				int t1 = i*n;
+				while (t1 != 0){
+					int t2 = t1 % 10;
+					myset.insert(t2);
+					t1 /= 10;
+				}
+				if (myset.size() == 10){
+					cout << "Case #" << z << ": " << i*n << endl;
+					break;
+				}
+			}
+		}
+			
+		// if (myset.size() < 10){
+		// 	cout << n << endl;
+		// 	cout << "size " << myset.size() << endl;
+		// }
+		
+	}
+}
